2c9a20c7d6dcf847555c7169bb933571a698ffc8
xia <654264879@qq.com>
2022-08-30 19:36:00 +0800

    second commit

--- src/main/java/com/atguigu/gitDemo.java
+++ src/main/java/com/atguigu/gitDemo.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("git test...");
+        System.out.println("hello git1");
     }
 }
