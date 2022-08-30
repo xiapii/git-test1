aa1e1321e72c3b580b6107b46315776e4c19b546
xia <654264879@qq.com>
2022-08-30 20:06:01 +0800

    master commit

--- src/main/java/com/atguigu/gitDemo.java
+++ src/main/java/com/atguigu/gitDemo.java
@@ -6,5 +6,6 @@ public class gitDemo {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("master test");
     }
 }
